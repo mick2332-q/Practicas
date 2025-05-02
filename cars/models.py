10f604edcebc0969fb4589961a37857ac4b67be3
Migue <ayalamigue908@gmail.com>
2025-05-02 18:25:52 -0500

    ultima clase

---
+++ cars/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Car(models.Model):
+    brand = models.CharField(max_length=100)       # Marca
+    model = models.CharField(max_length=100)       # Modelo
+    year = models.PositiveIntegerField()           # Año de fabricación
+    price = models.DecimalField(max_digits=10, decimal_places=2) # Precio
+    mileage = models.PositiveIntegerField()        # Kilometraje
+    description = models.TextField(blank=True)   # Descripción
+    image = models.ImageField(upload_to='car_images/', blank=True, null=True)
+    city = models.CharField(max_length=100)     # Municipio
+    state = models.CharField(max_length=100)    # Departamento
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.brand} {self.model} ({self.year})"
